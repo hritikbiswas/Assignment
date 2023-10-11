1e52eaf6a50f05386708700cee7b51ef1c02fe6a
hritikbiswas <hrittikbiswas90@gmail.com>
2023-10-11 15:51:25 +0530

    react assignment 4 is added

---
+++ react4.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UI with react</title>
+</head>
+<body>
+    <div id="root">
+  <h1></h1>
+    </div>
+</body>
+<script src="https://www.unpkg.com/react@18.2.0/umd/react.production.min.js"></script>
+<script src="https://www.unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js"></script>
+<script>
+       let rootElement=document.getElementById("root")
+       let element=React.createElement("div",{
+        className:"container",
+        children:"hello world",
+       })
+
+       let reactRoot=ReactDOM.createRoot(rootElement);
+       reactRoot.render(element)
+</script>
+</html>
